8652cdcfdbb431c9e272f135abff5cc0606a29e6
wendyww9 <wendyskystar@gmail.com>
2025-07-21 22:34:19 -0700

    first commit

---
+++ src/main/java/com/recipehub/backendrecipehub/BackEndRecipeHubApplication.java
@@ -0,0 +1,13 @@
+package com.recipehub.backendrecipehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRecipeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndRecipeHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recipehub/backendrecipehub/BackEndRecipeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipehub.backendrecipehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndRecipeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
